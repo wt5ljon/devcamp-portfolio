e7820ba62784ac579dcd3dc9a002eccc91a8b248
Jon Olson <jonolson64@outlook.com>
2017-05-08 20:22:11 -0400

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://wt5l-portfolio.herokuapp.com', 'http://wt5l-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://wt5l-portfolio.herokuapp.com/cable"
 end
